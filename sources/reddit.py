37220c03b6ef9e2f9a2e25da71e8a03c9bd98312
root <spmassot@gmail.com>
2017-07-05 00:09:56 -0400

    makes some changes to reddit.py

--- sources/reddit.py
+++ sources/reddit.py
@@ -1,6 +1,7 @@
 import praw
 from collections import namedtuple
 from os import environ as env
+from pandas import DataFrame
 
 
 def get_client():
@@ -11,9 +12,12 @@ def get_client():
         user_agent=env['reddit_user_agent']
     )
 
-def get_subr_top(sub_name, n):
+def get_subr_top(sub_name, n=None):
     """Returns the top n submissions from r/sub_name"""
-    return get_client().subreddit(sub_name).top(limit=n)
+    if n:
+        return get_client().subreddit(sub_name).top(limit=n)
+    else:
+        return get_client().subreddit(sub_name).top()
 
 def get_comment_props(comment_in):
     """Expects a praw CommentForest object, returns a named tuple"""
@@ -24,10 +28,14 @@ def get_comment_props(comment_in):
         [cmnt.id for cmnt in comment_in.replies]
     )
 
-if __name__ == '__main__':
-    from pandas import DataFrame
-    for submission in get_subr_top('crappydesign', 1):
-        submission.comments.replace_more()
+def main(sub, limt=None):
+    """Grabs top submissions from a subreddit,
+    returns their comments as a dataframe"""
+    for submission in get_subr_top(sub, n=limt):
+        submission.comments.replace_more(limit=0)
         comms = [get_comment_props(x) for x in submission.comments.list()]
         frame = DataFrame(comms)
-    frame.to_csv('test_data.csv')
+    return frame
+    
+if __name__ == '__main__':
+    main('crappydesign',10).to_csv('test_data.csv')
